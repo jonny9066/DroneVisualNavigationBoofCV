7b991e7a9491a525e49678da0d619f368cbd9f90
JonTP <jon9028@gmail.com>
2023-06-11 18:23:05 +0300

    Made scene recognition class modular. Next: Incorporate mosiac and scene recognition and test

--- src/main/java/org/example/LocationFromMap.java
+++ src/main/java/org/example/LocationFromMap.java
@@ -33,11 +33,14 @@ import boofcv.io.image.ImageFileListIterator;
 import boofcv.io.image.UtilImageIO;
 import boofcv.io.recognition.RecognitionIO;
 import boofcv.misc.BoofMiscOps;
+import boofcv.struct.feature.Match;
 import boofcv.struct.image.GrayF32;
 import boofcv.struct.image.GrayU8;
 import boofcv.struct.image.ImageType;
 import boofcv.struct.image.Planar;
+import georegression.struct.point.Point2D_F64;
 import org.apache.commons.io.FilenameUtils;
+import org.bridj.util.Pair;
 import org.ddogleg.struct.DogArray;
 
 import java.awt.*;
@@ -61,41 +64,44 @@ import java.util.Random;
  */
 public class LocationFromMap {
 	private static final int NUM_TEST_IMAGES = 16;
-	public static void main( String[] args ) {
-//		String imagePath = "resources/example/recognition/scene"; //UtilIO.pathExample("recognition/scene");
-		String imageTrainPath = "resources/for_scene/split_train";
-		String imageTestPath = "resources/for_scene/split_test";
-		List<String> imagesTrain = UtilIO.listByPrefix(imageTrainPath, null, ".png");
-		List<String> imagesTest = UtilIO.listByPrefix(imageTestPath, null, ".png");
-		Collections.sort(imagesTrain);
-		Collections.sort(imagesTest);
+	private ImageType<GrayU8> imageType = ImageType.SB_U8;
 
-		SceneRecognition<GrayU8> recognizer;
+	SceneRecognition<GrayU8> recognizer;// the scene recognition object
+	private File saveDirectory;
+	ImageGridPanel guiMap;// gui for displaying map with red square for match
+	BufferedImage lastQueryImage;
+
+
+//	public getLocationFromImage(){
+//
+//	}
+
+
+	public LocationFromMap(){
+		String imageTrainPath = "resources/for_scene/split_train";
+		List<String> imagesTrain = UtilIO.listByPrefix(imageTrainPath, null, ".png");
+		Collections.sort(imagesTrain);
+
 
 		// Except for real-time applications or when there are more than a few hundred images, you might want to
 		// just learn the dictionary from scratch
-		var saveDirectory = new File("example_recognition");
+		File saveDirectory = new File("example_recognition");
 
 		// Tell it to process gray U8 images
 		ImageType<GrayU8> imageType = ImageType.SB_U8;
 
 		// Used to look up images one at a time from various sources. In this case a list of images.
-		var imageTrainIterator = new ImageFileListIterator<>(imagesTrain, imageType);
-		var imageTestIterator = new ImageFileListIterator<>(imagesTest, imageType);
+		var imageTrainIterator =
+				new ImageFileListIterator<>(imagesTrain, imageType);
 
-		if (false) {
-			// Set the line above to true and it will download a pre-built model. Useful when you have a lot of images
-			// or simply want to skip the learning step
-			System.out.println("Downloading pre-built model");
-			recognizer = RecognitionIO.downloadDefaultSceneRecognition(new File("downloaded_models"), imageType);
-			recognizer.setVerbose(System.out, BoofMiscOps.hashSet(BoofVerbose.RECURSIVE));
-		}
 
-		else if (saveDirectory.exists()) {
+		// load pre-trained model if exists
+		if (saveDirectory.exists()) {
 			System.out.println("Loading previously generated model");
 			recognizer = RecognitionIO.loadFeatureToScene(saveDirectory, imageType);
 			recognizer.setVerbose(System.out, BoofMiscOps.hashSet(BoofVerbose.RECURSIVE));
 		}
+		// else, train a new model based on provided images
 		else {
 			// If many applications, learning a new model is a small fraction of the compute time and since its
 			// fit to the images it will be more accurate than a generic pre-built model
@@ -111,11 +117,12 @@ public class LocationFromMap {
 
 			// Learn the model from the initial set of images
 			recognizer.learnModel(imageTrainIterator);
-		}
 
-		// See if the recognition algorithm already has images loaded in to it
-		if (recognizer.getImageIds(null).isEmpty()) {
+			// the model also requires images to be in the database to match against them
+
 			// Add images to the database
+			// new model should be empty
+			if(!recognizer.getImageIds(null).isEmpty()) throw new RuntimeException("expected new model to be empty");
 			System.out.println("Adding images to the database");
 			imageTrainIterator.reset();
 			while (imageTrainIterator.hasNext()) {
@@ -129,29 +136,42 @@ public class LocationFromMap {
 					(WrapFeatureToSceneRecognition<GrayU8, ?>)recognizer, saveDirectory), "");
 		}
 
-		var gui = new ListDisplayPanel();
-		var gui2 = new ImageGridPanel(1, 1);// yoni: another gui for drawing image
-
-		// set image of whole map in second column
+		// create gui to display map and later squares of matches on it
+		guiMap = new ImageGridPanel(1, 1);
 		BufferedImage wholeMap = UtilImageIO.loadImageNotNull("resources/for_scene/map_scene.jpg");
-		Planar<GrayF32> mapFmt =
-				ConvertBufferedImage.convertFromPlanar(wholeMap, null, true, GrayF32.class);
-		gui2.setImage(0, 0, wholeMap);
-		gui2.setPreferredSize(new Dimension(mapFmt.width, mapFmt.height));
+		guiMap.setImage(0, 0, wholeMap);
+		guiMap.setPreferredSize(new Dimension(wholeMap.getWidth(), wholeMap.getHeight()));
 
-		//yoni: sample random test image
-		Random random = new Random();
 
-		int queryInd = random.nextInt(imagesTest.size());
-		// Specifies which image it will try to look up. In the example, related images are in sets of 3.
-//		int queryImage = 9;
 
+	}
+
+	public void displayMatchOnMap(SceneRecognition.Match bestMatch){
+		// get map pixel coordinates from filename of best match
+		String bestMatchDir = bestMatch.id;
+		String name = FilenameUtils.getBaseName(new File(bestMatchDir).getName());
+		String[] coordStr = name.split("_")[1].split("-");
+		int[] coordinates = Arrays.stream(coordStr).mapToInt(Integer::parseInt).toArray();
+		System.out.println("got pixel coordinates: " +Arrays.toString(coordinates));
+		int tlx = coordinates[0]; int tly = coordinates[1];
+		int brx = coordinates[2]; int bry = coordinates[3];
+
+		// draw square over map
+		// draw a red quadrilateral around the current frame in the mosaic
+		Graphics2D g2 = guiMap.getImage(0, 0).createGraphics();
+		g2.setColor(Color.RED);
+		g2.drawLine(tlx, tly, brx, tly); // top line
+		g2.drawLine(tlx,bry,brx,bry); // bottom line
+		g2.drawLine(tlx,tly, tlx, bry); // left line
+		g2.drawLine(brx,tly,brx,bry); // right line
+		guiMap.repaint();
+		ShowImages.showWindow(guiMap, "match on map", true);
+	}
+	public void displayClosestMatches(DogArray<SceneRecognition.Match> matches, String queryName){
+		ListDisplayPanel gui = new ListDisplayPanel();
 		// Add the target which the other images are being matched against
-		gui.addImage(UtilImageIO.loadImageNotNull(imagesTest.get(queryInd)), "Query "+ FilenameUtils.getBaseName(imagesTest.get(queryInd)), ScaleOptions.ALL);
+		gui.addImage(lastQueryImage, "Query "+ queryName, ScaleOptions.ALL);
 
-		// Look up images
-		var matches = new DogArray<>(SceneRecognition.Match::new);
-		recognizer.query(imageTestIterator.loadImage(queryInd),/* filter */ ( id ) -> true,/* limit */ 5, matches);
 		for (int i = 0; i < matches.size; i++) {
 			String file = matches.get(i).id;
 			double error = matches.get(i).error;
@@ -160,28 +180,53 @@ public class LocationFromMap {
 			gui.addImage(image, String.format("%20s Error %6.3f", name, error), ScaleOptions.ALL);
 		}
 
-		// get pixel coordinates of best match on whole map
-		String bestMatch = matches.get(0).id;
-		String name = FilenameUtils.getBaseName(new File(bestMatch).getName());
-		String[] coordStr = name.split("_")[1].split("-");
-		int[] coordinates = Arrays.stream(coordStr).mapToInt(Integer::parseInt).toArray();
-		System.out.println("got pixel coordinates: " +Arrays.toString(coordinates));
-		int tlx = coordinates[0]; int tly = coordinates[1];
-		int brx = coordinates[2]; int bry = coordinates[3];
-		// draw square over map
-		// draw a red quadrilateral around the current frame in the mosaic
-		Graphics2D g2 = gui2.getImage(0, 0).createGraphics();
-		g2.setColor(Color.RED);
-		g2.drawLine(tlx, tly, brx, tly); // top line
-		g2.drawLine(tlx,bry,brx,bry); // bottom line
-		g2.drawLine(tlx,tly, tlx, bry); // left line
-		g2.drawLine(brx,tly,brx,bry); // right line
-//		gui.repaint();
-		ShowImages.showWindow(gui2, "match on map", true);
-
-		System.out.println("Train Images Num = " + imagesTrain.size());
-		System.out.println(imagesTest.get(queryInd) + " -> " + matches.get(0).id + " matches.size=" + matches.size);
-
 		ShowImages.showWindow(gui, "Similar Images by Features", true);
+	}
+
+	public DogArray<SceneRecognition.Match> getMatchesArray(BufferedImage queryBufferedImage){
+		lastQueryImage = queryBufferedImage;
+        GrayU8 queryImage = ConvertBufferedImage.convertFrom(queryBufferedImage, (GrayU8) null);
+
+		// Look up images
+		DogArray<SceneRecognition.Match> matches = new DogArray<>(SceneRecognition.Match::new);
+
+//		recognizer.query(imageTestIterator.loadImage(queryInd),/* filter */ ( id ) -> true,/* limit */ 5, matches);
+
+		recognizer.query(queryImage, /* filter */ ( id ) -> true,/* limit */ 5, matches);
+
+		return  matches;
+//		System.out.println("Train Images Num = " + imagesTrain.size());
+//		System.out.println(imagesTest.get(queryInd) + " -> " + matches.get(0).id + " matches.size=" + matches.size);
+
+//		return new Pair<>(.0,.0);
+	}
+
+
+	public static void main( String[] args ) {
+		// create a scene matching object
+		LocationFromMap locationFromMap = new LocationFromMap();
+
+		// take an image from some test directory
+		String imageTestPath = "resources/for_scene/split_test";
+		List<String> imagesTest = UtilIO.listByPrefix(imageTestPath, null, ".png");
+		Collections.sort(imagesTest);
+		// sample random test image
+		Random random = new Random();
+		int queryInd = random.nextInt(imagesTest.size());
+
+		String queryImageDir = imagesTest.get(queryInd);
+		BufferedImage queryImage = UtilImageIO.loadImageNotNull(queryImageDir);
+		var matches = locationFromMap.getMatchesArray(queryImage);
+
+//		locationFromMap.displayClosestMatches(matches ,FilenameUtils.getBaseName(queryImageDir));
+		locationFromMap.displayMatchOnMap(matches.get(0));
+		locationFromMap.displayMatchOnMap(matches.get(1));
+
+
+		// Specifies which image it will try to look up. In the example, related images are in sets of 3.
+//		int queryImage = 9;
+
+
+
 	}
 }
